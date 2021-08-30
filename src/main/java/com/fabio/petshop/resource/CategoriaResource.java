aeda5ab7d7b26facc3aa1304286b4a92addbcc8f
Fábio Cordeiro <fabioled@gmail.com>
2021-08-30 18:04:43 -0300

    Criando Repository e Service para Categoria

--- src/main/java/com/fabio/petshop/resource/CategoriaResource.java
+++ src/main/java/com/fabio/petshop/resource/CategoriaResource.java
@@ -3,27 +3,27 @@ package com.fabio.petshop.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fabio.petshop.domain.Categoria;
+import com.fabio.petshop.service.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-
-			Categoria cat1 = new Categoria(1, "Alimento");
-			Categoria cat2 = new Categoria(1, "Limpeza");
-		
-			List<Categoria> lista = new ArrayList<Categoria>();
-			lista.add(cat1);
-			lista.add(cat2);
-			
-			return lista;			
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id){
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 			
 	}	
 	
