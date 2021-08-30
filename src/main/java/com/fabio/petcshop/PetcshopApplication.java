7b08403b05db167c180f368408ca52529edc1a18
Fábio Cordeiro <fabioled@gmail.com>
2021-08-30 11:29:16 -0300

    Projeto criado

---
+++ src/main/java/com/fabio/petcshop/PetcshopApplication.java
@@ -0,0 +1,13 @@
+package com.fabio.petcshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetcshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabio/petcshop/PetcshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabio.petcshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
