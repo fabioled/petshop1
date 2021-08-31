ded13fa39a69916d3dc46d087d1194be86bcf432
Fábio Cordeiro <fabioled@gmail.com>
2021-08-30 21:08:15 -0300

    Criando instâncias com PopulaDados

--- src/main/java/com/fabio/petshop/PetcshopApplication.java
+++ src/main/java/com/fabio/petshop/PetcshopApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetcshopApplication {
+public class PetcshopApplication  {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetcshopApplication.class, args);
+		SpringApplication.run(PetcshopApplication.class, args);		
+		
 	}
 
 }
