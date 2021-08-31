24440054e712824381b28fa2475138d8ba707b36
Fábio Cordeiro <fabioled@gmail.com>
2021-08-30 22:45:34 -0300

    Ajustesno endpoint /categorias/{id}, corrigindo referencia ciclica e tratamento de exception

--- src/main/java/com/fabio/petshop/service/CategoriaService.java
+++ src/main/java/com/fabio/petshop/service/CategoriaService.java
@@ -7,17 +7,19 @@ import org.springframework.stereotype.Service;
 
 import com.fabio.petshop.domain.Categoria;
 import com.fabio.petshop.repository.CategoriaRepository;
+import com.fabio.petshop.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
-		@Autowired
-		private CategoriaRepository repo; 
-		
-		public Categoria find(Integer id) {
-			Optional<Categoria> obj = repo.findById(id);
-			return obj.orElse(null);
-		}	
-		
-	
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"objeto não encotrado! ID: " + id + ", Tipo: " + Categoria.class.getName()));
+
+	}
+
 }
--- src/main/java/com/fabio/petshop/utils/PopulaDados.java
+++ src/main/java/com/fabio/petshop/utils/PopulaDados.java
@@ -29,17 +29,17 @@ public class PopulaDados {
 		Categoria cat3 = new Categoria(null, "Cosmético");
 		
 		Produto p1 = new Produto(null, "Ração", 100.0);
-		Produto p2 = new Produto(null, "Vermifugo", 10.0);
-		Produto p3 = new Produto(null, "Sache", 10.0);
+		Produto p2 = new Produto(null, "Sache", 10.0);
+		Produto p3 = new Produto(null, "Vermifugo", 20.0);		
 		Produto p4 = new Produto(null, "Shampoo", 50.0); 
 		
-		cat1.getProdutos().addAll(Arrays.asList(p1, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2, p4));		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2));
+		cat2.getProdutos().addAll(Arrays.asList(p3, p4));		
 		cat3.getProdutos().addAll(Arrays.asList(p4));		
 
 		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat2));
 		p4.getCategorias().addAll(Arrays.asList(cat2, cat3));
 				
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
